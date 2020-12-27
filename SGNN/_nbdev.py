ee06fdf7305ce8cec9fb92c8fa4b49bd3d0a0de1
glicerico <suarezandres@gmail.com>
2020-12-27 10:00:52 -0600

    Fix dimensions in randombinaryprojections

--- SGNN/_nbdev.py
+++ SGNN/_nbdev.py
@@ -4,12 +4,30 @@ __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
 index = {"data_filepath": "00_core.ipynb",
          "data": "00_core.ipynb",
+         "y": "00_core.ipynb",
+         "X": "00_core.ipynb",
+         "le": "00_core.ipynb",
+         "y_train": "00_core.ipynb",
+         "y_test": "00_core.ipynb",
          "char_ngram_range": "00_core.ipynb",
          "char_term_frequency_params": "00_core.ipynb",
          "T": "00_core.ipynb",
          "d": "00_core.ipynb",
          "rand_seeds": "00_core.ipynb",
-         "hashing_feature_union_params": "00_core.ipynb"}
+         "hashing_feature_union_params": "00_core.ipynb",
+         "MyRBP": "00_core.ipynb",
+         "params": "00_core.ipynb",
+         "preprocessor": "00_core.ipynb",
+         "union_pipe": "00_core.ipynb",
+         "train_labels": "00_core.ipynb",
+         "test_labels": "00_core.ipynb",
+         "train_features": "00_core.ipynb",
+         "model": "00_core.ipynb",
+         "lr_schedule": "00_core.ipynb",
+         "opt": "00_core.ipynb",
+         "EPOCHS": "00_core.ipynb",
+         "BATCH_SIZE": "00_core.ipynb",
+         "test_features": "00_core.ipynb"}
 
 modules = ["core.py"]
 
