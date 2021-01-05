94167e76b19fd5e7f111a85eead2109cea7bae69
glicerico <suarezandres@gmail.com>
2021-01-04 19:42:25 -0600

    Some docu fixes

--- SGNN/_nbdev.py
+++ SGNN/_nbdev.py
@@ -13,7 +13,9 @@ index = {"import_data": "00_core.ipynb",
          "LOWERCASE": "00_core.ipynb",
          "CLEAN": "00_core.ipynb",
          "REMOVE_CONTINUATIONS": "00_core.ipynb",
-         "data_filepath": "00_core.ipynb"}
+         "TRAIN_FILEPATH": "00_core.ipynb",
+         "VALID_FILEPATH": "00_core.ipynb",
+         "TEST_FILEPATH": "00_core.ipynb"}
 
 modules = ["core.py"]
 
